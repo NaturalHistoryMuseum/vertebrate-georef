fd216222aa864438f20a5b9be11ca9b28f6f7e70
Natalie Cooper <ncooper@tcd.ie>
2018-05-15 14:21:33 +0100

    Modify colors for colour blind and black and white printing

--- analyses/01-pilot-data-plots.R
+++ analyses/01-pilot-data-plots.R
@@ -3,6 +3,7 @@
 # Load libraries
 library(tidyverse)
 library(ggmap)
+library(viridisLite)
 
 # Read in the data
 # This is not yet properly validated and needs some tidying
@@ -16,11 +17,12 @@ worldmap <- map_data("world")
 ggplot(data = worldmap, aes(x = long, y = lat, group = group)) + 
   geom_polygon(fill = NA, col = "black", size = 0.1) +
   geom_point(data = amphibians, aes(x = Longitude, y = Latitude, 
-                                    group = NA, col = Order), 
-             size = 0.5, alpha = 0.8) +
+                                    group = NA, color = Order, shape = Order), 
+             size = 1, alpha = 1) +
   theme_void() +
   theme(legend.position = c(0.15, 0.3),
         legend.title=element_blank()) + 
+  scale_colour_manual(values =c(viridis(4)[1:3])) +
   guides(colour = guide_legend(override.aes = list(size=1),
                                keyheight = 0.8, keywidth = 0.5))
 
