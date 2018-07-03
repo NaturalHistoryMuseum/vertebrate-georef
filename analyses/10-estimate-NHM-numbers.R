a0ad63a5acf75ef9afa920c1aee44e9f1141a48c
Natalie Cooper <ncooper@tcd.ie>
2018-07-03 12:36:10 +0100

    Script to estimate numbers of specimens and species pre 1900 in NHM records

---
+++ analyses/10-estimate-NHM-numbers.R
@@ -0,0 +1,76 @@
+# Estimating numbers from NHM 
+
+library(tidyverse)
+library(readxl)
+
+# Read in data
+herp <- read_xlsx("data/NERC_georef/Amphibians_and_reptiles/May_2016_BMNH_herp_data.xlsx")
+bird <- read_csv("data/NERC_georef/Birds/occurrence.csv")
+mamm <- read_csv("data/NERC_georef/Mammals/occurrence.csv")
+fish <- read_csv("data/NERC_georef/Fishes/occurrence.csv")
+  
+# Select just ZOO specimens
+# Exclude stubs
+# Exclude post 1900
+# Exclude without species names
+# Exclude without locality data
+
+herp %>%
+  # Extract dates from registration numbers
+  mutate(year = as.numeric(str_extract(herp$RegRegistrationNumber, "\\d{4}"))) %>%
+  # Remove pre 1901 specimens
+  filter(year < 1901 & !is.na(year) & year > 1750) %>%
+  # Exclude without species names
+  filter(!is.na(DarScientificName)) %>%
+  # Exclude without locality data
+  filter(!is.na(DarLocality)) %>%
+  # Split into class and get total number of specimens
+  group_by(DarClass) %>%
+  summarise(n(),
+            length(unique(DarScientificName)))
+# NA class = Reptiles
+
+bird %>%
+  # Extract dates from registration numbers
+  mutate(year = as.numeric(str_extract(bird$catalogNumber, "\\d{4}"))) %>%
+  # Extract only zoology records
+  filter(collectionCode == "ZOO") %>%
+  # Remove pre 1901 specimens
+  filter(year < 1901 & !is.na(year) & year > 1750) %>%
+  # Exclude without species names
+  filter(!is.na(scientificName)) %>%
+  # Exclude without locality data
+  filter(!is.na(locality)) %>%
+  # Get total number of specimens and species
+  summarise(n(),
+            length(unique(scientificName)))
+
+mamm %>%
+  # Extract dates from registration numbers
+  mutate(year = as.numeric(str_extract(mamm$catalogNumber, "\\d{4}"))) %>%
+  # Extract only zoology records
+  filter(collectionCode == "ZOO") %>%
+  # Remove pre 1901 specimens
+  filter(year < 1901 & !is.na(year) & year > 1750) %>%
+  # Exclude without species names
+  filter(!is.na(scientificName)) %>%
+  # Exclude without locality data
+  filter(!is.na(locality)) %>%
+  # Get total number of specimens and species
+  summarise(n(),
+            length(unique(scientificName)))
+
+fish %>%
+  # Extract dates from registration numbers
+  mutate(year = as.numeric(str_extract(fish$catalogNumber, "\\d{4}"))) %>%
+  # Extract only zoology records
+  filter(collectionCode == "ZOO") %>%
+  # Remove pre 1901 specimens
+  filter(year < 1901 & !is.na(year) & year > 1750) %>%
+  # Exclude without species names
+  filter(!is.na(scientificName)) %>%
+  # Exclude without locality data
+  filter(!is.na(locality)) %>%
+  # Get total number of specimens and species
+  summarise(n(),
+            length(unique(scientificName)))
