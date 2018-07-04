0609d551dcfb003c226e3d340711e6f71945f958
Natalie Cooper <ncooper@tcd.ie>
2018-07-04 14:49:08 +0100

    Edits to change species labels

--- analyses/08-pilot-eoo-ggplot.R
+++ analyses/08-pilot-eoo-ggplot.R
@@ -39,25 +39,21 @@ maps <- readOGR("data/CAUDATA")
 # Subset maps for the three species
 maps_noto <- subset(maps, maps@data$binomial == "Notophthalmus viridescens")
 maps_tg <- subset(maps, maps@data$binomial == "Taricha granulosa")
-#maps_tt <- subset(maps, maps@data$binomial == "Taricha torosa")
-
-# Subset gbif to the three specis
-gbif_three <- gbif %>%
-  filter(#species == "Taricha torosa" | 
-         species == "Taricha granulosa" |
-        species == "Notophthalmus viridescens" )
 
 # Make individual species datasets for hulls
 gbif_noto <- gbif %>%
-  filter(species == "Notophthalmus viridescens")
+  filter(species == "Notophthalmus viridescens") %>%
+  mutate(species = str_replace(species, "Notophthalmus ", "N."))
 
 gbif_tg <- gbif %>%
-  filter(species == "Taricha granulosa")
+  filter(species == "Taricha granulosa") %>%
+  mutate(species = str_replace(species, "Taricha ", "T."))
 
-# Make hulls for three species
+gbif_two <- rbind(gbif_noto, gbif_tg)
+
+# Make hulls for each species
 hull_noto <- make.hull(gbif_noto)
 hull_tg <- make.hull(gbif_tg)
-#hull_tt <- make.hull(gbif_tt)
 
 #---------------------------------------------------------------
 # Plot maps
@@ -65,18 +61,16 @@ ggplot(data = usamap, aes(x = long, y = lat, group = group)) +
   geom_polygon(fill = NA, col = "black", size = 0.5) +
   geom_polygon(data = hull_noto, aes(x = x, y = y, group = NULL), alpha = 0.2) +
   geom_polygon(data = hull_tg, aes(x = x, y = y, group = NULL), alpha = 0.2) +
-  #geom_polygon(data = hull_tt, aes(x = x, y = y, group = NULL), alpha = 0.2) +
-  geom_point(data = gbif_three, aes(x = decimallongitude, y = decimallatitude, 
+  geom_point(data = gbif_two, aes(x = decimallongitude, y = decimallatitude, 
                                    group = NA, shape = species, color = species), 
                                    alpha = 0.9) +
   scale_colour_manual(values = c(viridis(4)[1:3])) +
   theme_void() +
   theme(legend.position = c(0.35, 0.7),
-    #legend.position = "top",
         legend.title = element_blank(),
         legend.text = element_text(face = "italic")) + 
  guides(colour = guide_legend(override.aes = list(size = 2),
-                               keyheight = 0.8, keywidth = 0.5))
+                              keyheight = 0.8, keywidth = 0.5))
 
-ggsave("outputs/convex-hulls-no-torosa.png", height = 5, width = 8, 
+ggsave("outputs/convex-hulls.png", height = 5, width = 8, 
        units = c("cm"))
