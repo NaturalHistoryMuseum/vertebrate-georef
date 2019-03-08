de825ea0ac05bdc4323b856a0c13c455741dcfb1
Natalie Cooper <ncooper@tcd.ie>
2019-03-08 13:27:07 +0000

    Tidy code

--- analyses/gbif-overlaps.R
+++ analyses/gbif-overlaps.R
@@ -40,23 +40,25 @@ convert.spdf <- function(map) {
   map.df <- dplyr::full_join(map.points, map@data, by = "id")
   return(map.df)
 }
-```
 
-```{r, echo = FALSE, message = FALSE, warning = FALSE, error = FALSE}
+#-----------------------------
+# Add data and maps
+#-----------------------------
 # Read in the tidied GBIF data
 gbif <- read_csv(here("data/tidy-gbif.csv"))
-```
 
-```{r}
 # Read in maps
-# This takes a long time and a lot of memory so I recommend doing one clade at a time
+# This takes a long time and a lot of memory so I 
+# recommend doing one clade at a time
+
 #maps_amphibian <- readOGR(here("data/AMPHIBIANS"))
 #maps_mammal <- readOGR(here("data/MAMMALS"))
 #maps_bird <- readOGR("data/BIRDS")
-```
 
-```{r, echo = FALSE}
-# First find only those species with >= 3 specimens with unique localities
+#-----------------------------
+# Wrangle the data a bit
+#-----------------------------
+# Select only those species with >= 3 specimens with unique localities
 specimen3 <- 
   gbif %>%
   distinct(clade, binomial, decimallongitude, decimallatitude) %>%
@@ -65,11 +67,8 @@ specimen3 <-
   filter(n >= 3)
 
 # Then select these specimens from the gbif data
-gbif3 <- gbif2[gbif2$binomial %in% specimen3$binomial, ]
-```
+gbif3 <- gbif[gbif$binomial %in% specimen3$binomial, ]
 
-
-```{r, echo = FALSE}
 # Split by clade
 gbif3_amphibian <- 
   gbif3 %>%
@@ -83,75 +82,69 @@ gbif3_mammal <-
   gbif3 %>%
   filter(clade == "Mammals")
 
-```
+# Convert to spatial points dataframe for plotting
+dsSPDF_amphibian <- SpatialPointsDataFrame(gbif3_amphibian[, c(10,11)], 
+                                           data.frame(gbif3_amphibian[, 1:11]),
+                                           proj4string = CRS("+proj=longlat +datum=WGS84 
+                                                             +no_defs +ellps=WGS84 
+                                                             +towgs84=0,0,0"))
+
+dsSPDF_bird <- SpatialPointsDataFrame(gbif3_bird[, c(10,11)], 
+                                      data.frame(gbif3_bird[, 1:11]),
+                                      proj4string = CRS("+proj=longlat +datum=WGS84 
+                                                        +no_defs +ellps=WGS84 
+                                                        +towgs84=0,0,0"))
+
+dsSPDF_mammal <- SpatialPointsDataFrame(gbif3_mammal[, c(10,11)], 
+                                        data.frame(gbif3_mammal[, 1:11]),
+                                        proj4string = CRS("+proj=longlat +datum=WGS84 
+                                                          +no_defs +ellps=WGS84 
+                                                          +towgs84=0,0,0"))
 
 
-```{r}
+#-----------------------------
+# Convex hulls
+#-----------------------------
 # Create convex hulls for all species with >= 3 unique localities
-hull_species_amphibian <- list()
-hull_species_bird <- list()
-hull_species_mammal <- list()
 
-# Make hulls                              
+# Amphibians
+# Make empty output list
+hull_species_amphibian <- list()     
+# Create hulls for each species
 for(i in 1:length(unique(gbif3_amphibian$binomial))){
   hull_species_amphibian[[i]] <- make.hull(gbif3_amphibian[gbif3_amphibian$binomial ==         
                                                           unique(gbif3_amphibian$binomial)[i], ])
 }
+# Add species names
+names(hull_species_amphibian) <- unique(gbif3_amphibian$binomial)
 
+# Birds
+hull_species_bird <- list()
 for(i in 1:length(unique(gbif3_bird$binomial))){
   hull_species_bird[[i]] <- make.hull(gbif3_bird[gbif3_bird$binomial ==         
                                                 unique(gbif3_bird$binomial)[i], ])
 }
+names(hull_species_bird) <- unique(gbif3_bird$binomial)
 
+# Mammals
+hull_species_mammal <- list()
 for(i in 1:length(unique(gbif3_mammal$binomial))){
   hull_species_mammal[[i]] <- make.hull(gbif3_mammal[gbif3_mammal$binomial ==         
                                                         unique(gbif3_mammal$binomial)[i], ])
 }
-
-# Add species names
-names(hull_species_amphibian) <- unique(gbif3_amphibian$binomial)
-names(hull_species_bird) <- unique(gbif3_bird$binomial)
 names(hull_species_mammal) <- unique(gbif3_mammal$binomial)
-```
 
-```{r, echo = FALSE}
-# Convert to spatial points dataframe
-dsSPDF_amphibian <- SpatialPointsDataFrame(gbif3_amphibian[, c(10,11)], 
-                                           data.frame(gbif3_amphibian[, 1:11]),
-                                           proj4string = CRS("+proj=longlat +datum=WGS84 
-                                                              +no_defs +ellps=WGS84 
-                                                              +towgs84=0,0,0"))
 
-dsSPDF_bird <- SpatialPointsDataFrame(gbif3_bird[, c(10,11)], 
-                                           data.frame(gbif3_bird[, 1:11]),
-                                           proj4string = CRS("+proj=longlat +datum=WGS84 
-                                                              +no_defs +ellps=WGS84 
-                                                              +towgs84=0,0,0"))
-
-dsSPDF_mammal <- SpatialPointsDataFrame(gbif3_mammal[, c(10,11)], 
-                                           data.frame(gbif3_mammal[, 1:11]),
-                                           proj4string = CRS("+proj=longlat +datum=WGS84 
-                                                              +no_defs +ellps=WGS84 
-                                                              +towgs84=0,0,0"))
-
-```
-
-```{r}
+#-----------------------------
+# Point overlaps
+#-----------------------------
 # Loop to extract overlaps
 # Will write functions for this at some stage
 
-# Create arrays for results
+# Create array for results
 overlaps_amphibian <- array(NA, dim = c(length(unique(dsSPDF_amphibian@data$binomial)), 4))
 colnames(overlaps_amphibian) <- c("Binomial", "NumberSpecimens", "NumberOverlaps", "PercentOverlaps")
 overlaps_amphibian <- data.frame(overlaps_amphibian)
-
-overlaps_bird <- array(NA, dim = c(length(unique(dsSPDF_bird@data$binomial)), 4))
-colnames(overlaps_bird) <- c("Binomial", "NumberSpecimens", "NumberOverlaps", "PercentOverlaps")
-overlaps_bird <- data.frame(overlaps_bird)
-
-overlaps_mammal <- array(NA, dim = c(length(unique(dsSPDF_mammal@data$binomial)), 4))
-colnames(overlaps_mammal) <- c("Binomial", "NumberSpecimens", "NumberOverlaps", "PercentOverlaps")
-overlaps_mammal <- data.frame(overlaps_mammal)
 
 # Loop through for each species
 for (i in 1:length(unique(dsSPDF_amphibian@data$binomial))) {
@@ -176,10 +169,16 @@ for (i in 1:length(unique(dsSPDF_amphibian@data$binomial))) {
   overlaps_amphibian[i, "PercentOverlaps"] <- (no_overlaps/no_types) * 100
   
 } # end loop
-```
 
-```{r, eval = FALSE}
+# Save output
+write_csv(overlaps_amphibian, path = here("data/overlaps_amphibian.csv"))
+#-----------------------------
 # Birds
+
+overlaps_bird <- array(NA, dim = c(length(unique(dsSPDF_bird@data$binomial)), 4))
+colnames(overlaps_bird) <- c("Binomial", "NumberSpecimens", "NumberOverlaps", "PercentOverlaps")
+overlaps_bird <- data.frame(overlaps_bird)
+
 # Loop through for each species
 for (i in 1:length(unique(dsSPDF_bird@data$binomial))) {
   
@@ -204,11 +203,12 @@ for (i in 1:length(unique(dsSPDF_bird@data$binomial))) {
   
 } # end loop
 
-```
+#-----------------------------
+# Mammals
 
-```{r}
-
-#maps_mammal <- readOGR(here("data/MAMMALS"))
+overlaps_mammal <- array(NA, dim = c(length(unique(dsSPDF_mammal@data$binomial)), 4))
+colnames(overlaps_mammal) <- c("Binomial", "NumberSpecimens", "NumberOverlaps", "PercentOverlaps")
+overlaps_mammal <- data.frame(overlaps_mammal)
 
 # Loop through for each species
 for (i in 1:length(unique(dsSPDF_mammal@data$binomial))) {
@@ -236,37 +236,94 @@ for (i in 1:length(unique(dsSPDF_mammal@data$binomial))) {
 
 write_csv(overlaps_mammal, path = here("data/overlaps_mammal.csv"))
 
-```
-
-```{r}
-# Look at the output
-overlaps_amphibian
-#overlaps_bird
-#overlaps_mammal
-
-# Save output
-write_csv(overlaps_amphibian, path = here("data/overlaps_amphibian.csv"))
-#write_csv(overlaps_bird, path = here("data/overlaps_bird.csv"))
-#write_csv(overlaps_mammal, path = here("data/overlaps_mammal.csv"))
-```
-
-
-```{r}
+#------------------------------------
+# Polygon overlaps i.e. convex hulls
+#-----------------------------------
 # Loop to extract area of overlap
 # Will write functions for this at some stage
 
+# Amphibians
 # Create arrays for results
 areas_amphibian <- array(NA, dim = c(length(unique(dsSPDF_amphibian@data$binomial)), 4))
 colnames(areas_amphibian) <- c("Binomial", "AreaHull", "AreaOverlaps", "PercentOverlaps")
 areas_amphibian <- data.frame(areas_amphibian)
 
+z <- 
+
+for (i in z) { # need to skip the ones with no map
+  
+  # Which species is species i?
+  species_i <- as.character(unique(dsSPDF_amphibian@data$binomial)[i])
+  # Select hull for species_i
+  hull_i <- hull_species_amphibian[[species_i]]
+  # Make hull a spatial polygon object
+  hull_poly_i <- SpatialPolygons(list(Polygons(list(Polygon(hull_i)), ID=1)))
+  
+  # Subset map data so we just have map for species i
+  map_i <- subset(maps_amphibian, maps_amphibian$binomial == species_i)
+  # Get intersection of map and hull
+  poly_overlap_i <- intersect(map_i, hull_poly_i)
+  
+  # Get areas
+  hull_area <- sum(areaPolygon(hull_poly_i))
+  map_area <- sum(areaPolygon(map_i))
+  overlap_area <- sum(areaPolygon(poly_overlap_i))
+  
+  # Outputs
+  # I have added the number and % of areas as some
+  # species have > 1 type
+  areas_amphibian[i, "Binomial"] <- species_i
+  areas_amphibian[i, "AreaHull"] <- hull_area
+  areas_amphibian[i, "AreaOverlaps"] <- overlap_area
+  areas_amphibian[i, "PercentOverlaps"] <- (overlap_area/hull_area) * 100
+  
+} # end loop
+
+write_csv(areas_amphibian, path = here("data/areas_amphibian.csv"))
+#-----------------------------------
+# Birds
 areas_bird <- array(NA, dim = c(length(unique(dsSPDF_bird@data$binomial)), 4))
 colnames(areas_bird) <- c("Binomial", "AreaHull", "AreaOverlaps", "PercentOverlaps")
 areas_bird <- data.frame(areas_bird)
 
+for (i in z) { # need to skip the ones with no map
+  
+  # Which species is species i?
+  species_i <- as.character(unique(dsSPDF_bird@data$binomial)[i])
+  # Select hull for species_i
+  hull_i <- hull_species_bird[[species_i]]
+  # Make hull a spatial polygon object
+  hull_poly_i <- SpatialPolygons(list(Polygons(list(Polygon(hull_i)), ID=1)))
+  
+  # Subset map data so we just have map for species i
+  map_i <- subset(maps_bird, maps_bird$binomial == species_i)
+  # Get intersection of map and hull
+  poly_overlap_i <- intersect(map_i, hull_poly_i)
+  
+  # Get areas
+  hull_area <- sum(areaPolygon(hull_poly_i))
+  map_area <- sum(areaPolygon(map_i))
+  overlap_area <- sum(areaPolygon(poly_overlap_i))
+  
+  # Outputs
+  # I have added the number and % of areas as some
+  # species have > 1 type
+  areas_bird[i, "Binomial"] <- species_i
+  areas_bird[i, "AreaHull"] <- hull_area
+  areas_bird[i, "AreaOverlaps"] <- overlap_area
+  areas_bird[i, "PercentOverlaps"] <- (overlap_area/hull_area) * 100
+  
+} # end loop
+
+write_csv(areas_bird, path = here("data/areas_bird.csv"))
+
+#-----------------------------------
+# Mammals
 areas_mammal <- array(NA, dim = c(length(unique(dsSPDF_mammal@data$binomial)), 4))
 colnames(areas_mammal) <- c("Binomial", "AreaHull", "AreaOverlaps", "PercentOverlaps")
 areas_mammal <- data.frame(areas_mammal)
+
+z <- 
 
 # Loop through for each species
 for (i in z) { # need to skip the ones with no map
@@ -298,14 +355,14 @@ for (i in z) { # need to skip the ones with no map
   
 } # end loop
 
-# Look at the output
-#areas_amphibian
-#areas_bird
-areas_mammal
-
-#write_csv(areas_amphibian, path = here("data/areas_amphibian.csv"))
-#write_csv(areas_bird, path = here("data/areas_bird.csv"))
 write_csv(areas_mammal, path = here("data/areas_mammal.csv"))
+
+
+
+
+
+
+
 ```
 
 ```{r}
